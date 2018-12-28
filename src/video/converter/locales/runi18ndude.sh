82bc5bedbc1b7a10e31ca0edcd7114a54933c789
alberto.duran <alberto.duran@upcnet.es>
2018-12-28 09:30:40 +0100

    Translations and restylish

--- src/video/converter/locales/runi18ndude.sh
+++ src/video/converter/locales/runi18ndude.sh
@@ -2,6 +2,6 @@
 
 DOMAIN='video.converter'
 
-/Users/alberto/Develop/python09102018/bin/i18ndude rebuild-pot --pot ${DOMAIN}.pot --create ${DOMAIN} ..
+/Users/alberto/Develop/py_po2mo/bin/i18ndude rebuild-pot --pot ${DOMAIN}.pot --create ${DOMAIN} ..
 #i18ndude merge --pot ${DOMAIN}.pot --merge ${DOMAIN}-manual.pot
-/Users/alberto/Develop/python09102018/bin/i18ndude sync --pot ${DOMAIN}.pot ./*/LC_MESSAGES/${DOMAIN}.po
+/Users/alberto/Develop/py_po2mo/bin/i18ndude sync --pot ${DOMAIN}.pot ./*/LC_MESSAGES/${DOMAIN}.po
